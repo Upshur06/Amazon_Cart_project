39443e839e23d45aedcc55222680c408978a778a
Upshur06 <mupshur2005@gmail.com>
2021-12-04 20:43:32 -0500

    add CartTotal component

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -4,7 +4,15 @@ import '../css/CartTotal.css'
 export const CartTotal = () => {
     return (
         <div className='CartTotal'>
-
+            <h3>
+                Subtotal(5 items):
+                <span className='CarTotal-price'>
+                    $2,172.38
+                </span>
+            </h3>
+            <button>
+                Proceed To Checkout
+            </button>
         </div>
     )
 }
